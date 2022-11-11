ba88b6803da2ca80af92ea8e0770a7c816361c43
Matt Boston <mjboston@hotmail.co.uk>
2022-11-11 16:21:51 +0000

    template and data

---
+++ README.md
@@ -0,0 +1,3 @@
+## Santa Tracker
+This an API that tracks santa location 7pm on the 24th and 7am on the 25th. 
+
--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+const DATA = require("./data.json");
 
+app.get("/", (req, res) => {
+  const startTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 19, 00, 00, 00);
+  const endTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 07, 00, 00, 00);
+  const time = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 24, 00, 00, 00);
+
+  // get the time
+  //   const time = new Date().toLocaleTimeString();
+
+  if (startTime < time && endTime > time) {
+    return res.status(400).json({ error: [{ msg: "Santa is sleeping, please try again later" }] });
+  }
+
+  //   determine where santa was before hand
+  //   console.log(endTime, time);
+  const beforeLocation = (Math.abs(new Date(endTime) - new Date(time)) / (1000 * 60)) % 60;
+  console.log(beforeLocation);
+  //   determine where santa is heading
+
+  // get long and lat of where sants is
+
+  res.json({ msg: "" });
+});
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
