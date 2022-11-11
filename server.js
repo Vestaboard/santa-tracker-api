09c50b0bd763fa4b4910511a70aa04d73eca015f
Matt Boston <mjboston@hotmail.co.uk>
2022-11-11 18:37:03 +0000

    Got time remaining

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const app = express();
-
+const { previousLocation } = require("./functions.js");
 const DATA = require("./data.json");
+
+const getLocations = (remaining) => {
+    
+
+};
 
 app.get("/", (req, res) => {
   const startTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 19, 00, 00, 00);
-  const endTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 07, 00, 00, 00);
-  const time = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 24, 00, 00, 00);
+  const endTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay() + 1, 07, 00, 00, 00);
+  const time = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 23, 00, 00, 00);
 
-  // get the time
-  //   const time = new Date().toLocaleTimeString();
-
-  if (startTime < time && endTime > time) {
+  if (time < startTime && time > endTime) {
     return res.status(400).json({ error: [{ msg: "Santa is sleeping, please try again later" }] });
   }
 
   //   determine where santa was before hand
-  //   console.log(endTime, time);
-  const beforeLocation = (Math.abs(new Date(endTime) - new Date(time)) / (1000 * 60)) % 60;
-  console.log(beforeLocation);
+  const timeRemaining = endTime - time / 1000;
+
+  const previousLocation = getLocations(timeRemaining);
   //   determine where santa is heading
 
   // get long and lat of where sants is
 
-  res.json({ msg: "" });
+  res.json({ msg: previousLocation });
 });
 app.listen(3000, () => {
   console.log("Server running on port 3000");
