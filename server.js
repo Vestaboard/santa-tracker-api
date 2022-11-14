fd393844c8fb0040e1badf75c9374de00b6ef2fd
Matt Boston <mjboston@hotmail.co.uk>
2022-11-14 15:20:53 +0000

    Time bug fix

--- server.js
+++ server.js
@@ -12,24 +12,26 @@ const getLocations = (start, end, remaining) => {
 
 app.get("/", (req, res) => {
   //Get times
-  const startTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 19, 00, 00, 00);
-  const endTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay() + 1, 07, 00, 00, 00);
-  const time = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), 23, 00, 00, 00);
+
+  const startTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 19, 00, 00, 00);
+  const endTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 1, 07, 00, 00, 00);
+  const time = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 19, 00, 00, 00);
 
   //   Validate Times
-  if (time < startTime && time > endTime) {
+  if (time < startTime || time > endTime) {
     return res.status(400).json({ error: [{ msg: "Santa is sleeping, please try again later" }] });
   }
 
   //  find where santa is
-  const timeRemaining = endTime - time / 1000;
+  const timeRemaining = (endTime - time) / 1000;
+
   const locations = getLocations(startTime, endTime, timeRemaining);
 
   //   determine where santa is heading
 
   // get long and lat of where sants is
 
-  res.json({ msg: locations });
+  res.json({ data: locations });
 });
 app.listen(3000, () => {
   console.log("Server running on port 3000");
